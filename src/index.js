32f00896e9a2a933dfa1c9ccd0c1faa563f288e7
Michal Dzialek <exodusofdead@gmail.com>
2019-11-02 16:57:56 +0100

    Added register code Log in class added Updated database and auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
+/* Selectors */
+
+  /* Sign up elements */
+  const registerEmail = document.querySelector('#signup_email');
+  const registerPassword = document.querySelector('#signup_password');
+  const registerPassword2 = document.querySelector('#signup_password_again');
+  const registerUsername = document.querySelector('#signup_username');
+  const registerFrom = document.querySelector('#signupform');
+  const registerModal = document.querySelector('#modal-signup');
+
+  /* log user in elements */
+
+/* Imports */
+import {AuthRegister, AuthLogin} from './modules/auth';
+
 /* Materialize initials code */
 document.addEventListener('DOMContentLoaded', () => {
-    let navigationElements = document.querySelectorAll('.sidenav'); //Mobile sidenav selector
-    M.Sidenav.init(navigationElements, {});//Mobile sidenav activator
+  let navigationElements = document.querySelectorAll('.sidenav'); //Mobile sidenav selector
+  M.Sidenav.init(navigationElements, {});//Mobile sidenav activator
 
-    let modalElements = document.querySelectorAll('.modal');
-    M.Modal.init(modalElements, {});
-  });
+  let modalElements = document.querySelectorAll('.modal');
+  M.Modal.init(modalElements, {});
+});
+
+/* Chatroom code */
+
+/* Register user part */
+registerFrom.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const Register = new AuthRegister(registerEmail.value.trim(), registerPassword.value, registerUsername.value)
+  Register.init();
+
+  registerFrom.reset();
+  M.Modal.getInstance(registerModal).close();
+});
+
+/* Log user in part */
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,1 +1,37 @@
-//2 classes, one for register, one for login
+/* Register new user in fireauth */
+class AuthRegister {
+    constructor(email, password, name){
+        this.email = email,
+        this.password = password,
+        this.name = name
+    }
+    init(){
+        auth.createUserWithEmailAndPassword(this.email, this.password) //creates user with email and password
+        .then( cred => {
+            const date = new Date(); //current date for timestamps
+            const newUser = {
+                username: this.name,
+                created: firebase.firestore.Timestamp.fromDate(date),
+                messages: 0
+            };
+            db.collection('users').doc(cred.user.uid).set(newUser).then().catch((error) => {console.log(error);}); //adds user additional data to database
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+};
+
+class AuthLogin {
+    constructor(email, password){
+        this.email = email,
+        this.password = password
+    }
+    init(){
+        auth.signInWithEmailAndPassword(this.email, this.password).then().catch((error) => {console.log(error);}); //Logs user in
+    }
+}
+
+
+/* Exports */
+export {AuthRegister, AuthLogin};
